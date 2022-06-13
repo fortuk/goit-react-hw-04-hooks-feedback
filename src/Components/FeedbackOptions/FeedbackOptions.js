d5d78b85e50b0a71fb9fe5f47b901c4b60a241f8
Алексей <fortuk22@rambler.ru>
2022-06-14 00:41:40 +0300

    hooks

--- src/Components/FeedbackOptions/FeedbackOptions.js
+++ src/Components/FeedbackOptions/FeedbackOptions.js
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import styles from './FeedbackOptions.module.css';
 
-const OptionsFeedback = ({ options, clickFeedback }) => {
+const FeedbackOptions = ({ options, clickFeedback }) => {
     return options.map((option) => ( <
         button key = { option }
         className = { styles.btn }
@@ -18,10 +18,10 @@ function makeFirstLetterBig(str) {
     return str[0].toUpperCase() + str.slice(1);
 }
 
-OptionsFeedback.propTypes = {
+FeedbackOptions.propTypes = {
     options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
     clickFeedback: PropTypes.func.isRequired,
 };
 
 
-export default OptionsFeedback;
+export default FeedbackOptions;
