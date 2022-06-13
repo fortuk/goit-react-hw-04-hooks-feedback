fe03515202e062d47432ca4730c69b13ab9b33ab
Алексей <fortuk22@rambler.ru>
2022-06-14 00:36:33 +0300

    hooks

--- src/Components/FeedbackOptions/FeedbackOptions.js
+++ src/Components/FeedbackOptions/FeedbackOptions.js
@@ -17,6 +17,7 @@ function makeFirstLetterBig(str) {
     if (!str) return str;
     return str[0].toUpperCase() + str.slice(1);
 }
+
 OptionsFeedback.propTypes = {
     options: PropTypes.arrayOf(PropTypes.string.isRequired).isRequired,
     clickFeedback: PropTypes.func.isRequired,
